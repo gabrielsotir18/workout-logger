63a97bc97ce7d7766ea3d1f24de1abd64e7fb5e1
gabriel sotir <gabrielsotir18@yahoo.com>
2023-10-07 23:43:36 +0300

    Add service classes

---
+++ src/main/java/com/workoutlogger/workoutlogger/service/WorkoutLoggerService.java
@@ -0,0 +1,20 @@
+package com.workoutlogger.workoutlogger.service;
+
+import com.workoutlogger.workoutlogger.DTO.WorkoutLogDTO;
+import com.workoutlogger.workoutlogger.entities.Exercise;
+import com.workoutlogger.workoutlogger.entities.User;
+import com.workoutlogger.workoutlogger.entities.WorkoutLog;
+
+import java.util.List;
+
+public interface WorkoutLoggerService {
+     void logExercise(WorkoutLogDTO workoutLogDTO);
+     void createExercise(Exercise exercise);
+     void createUser(User user);
+     WorkoutLogDTO getWorkoutLog(int id);
+     Exercise getExercise(int id);
+     List<Exercise> getAllExercises();
+     List<WorkoutLog> gettAllWorkoutLogsByUser(User user);
+
+
+}
