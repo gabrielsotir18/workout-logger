383b7f8eb89f2558391fc8af0bf87ca629a20faf
horia <horia.alex10@gmail.com>
2023-10-27 19:53:23 +0300

    add source code linting

--- frontend/app/components/Home.vue
+++ frontend/app/components/Home.vue
@@ -1,62 +1,73 @@
 <template>
-    <Page>
-        <ActionBar title="Workouts" />
+  <Page>
+    <ActionBar title="Workouts" />
 
-        <GridLayout columns="50, *, 50" rows="50, 100">
-            <Label class="fas" text.decode="&#xf060;" padding=10 textAlignment="center" backgroundColor="#c78f85" row="0" col="0" @tap="changeDate('decrease')"/>
-            <Label 
-                :text="humanReadableDate(date)" 
-                textAlignment="center" 
-                padding=10
-                row="0" 
-                col="1"
-                fontSize="24"
-                @tap="changeDate('now')" 
-                backgroundColor="#ffcfc7"
-            />
-            <Label class="fas" text.decode="&#xf061;" padding=10 row="0" col="2" backgroundColor="#c78f85" textAlignment="center" @tap="changeDate('increase')" />
-            <Label :text="curWorkout" row="1" col="1" textAlignment="center" fontSize="32" />
-        </GridLayout>
-    </Page>
+    <GridLayout columns="50, *, 50" rows="50, 100">
+      <Label
+        class="fas" text.decode="&#xf060;" padding="10"
+        text-alignment="center" background-color="#c78f85"
+        row="0" col="0" @tap="changeDate('decrease')"
+      />
+      <Label 
+        :text="humanReadableDate(date)" 
+        text-alignment="center" 
+        padding="10"
+        row="0" 
+        col="1"
+        font-size="24"
+        background-color="#ffcfc7" 
+        @tap="changeDate('now')"
+      />
+      <Label
+        class="fas" text.decode="&#xf061;" padding="10"
+        row="0" col="2"
+        background-color="#c78f85" text-alignment="center" @tap="changeDate('increase')"
+      />
+      <Label
+        :text="curWorkout" row="1" col="1"
+        text-alignment="center" font-size="32"
+      />
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
 import WorkoutService from '../services/WorkoutService.js'
 
 export default {
-    data() {
-        return {
-            workouts: WorkoutService.getWorkouts(),
-            date: new Date()
-        };
-    },
-    computed: {
-        curWorkout() {
-            const workout = WorkoutService.getWorkoutByDate(this.humanReadableDate(this.date))
-            return workout ? workout.title : 'No workout'
-        }
-    },
-    methods: {
-        printDate(item) {
-            console.log(this.humanReadableDate(item.date))
-        },
-        humanReadableDate(timestamp) {
-            return `${timestamp.getFullYear()}-${timestamp.getMonth() + 1}-${timestamp.getDate()}`
-        },
-        changeDate(mode) {
-            let newDate
-            if (mode === 'now') {
-                newDate = new Date()
-            } else {
-                newDate = new Date(this.date)
-                if (mode === 'decrease') {
-                    newDate.setDate(newDate.getDate() - 1)
-                } else if (mode === 'increase') {
-                    newDate.setDate(newDate.getDate() + 1)
-                }
-            }
-            this.date = newDate
-        },
+  data() {
+    return {
+      workouts: WorkoutService.getWorkouts(),
+      date: new Date()
     }
-};
+  },
+  computed: {
+    curWorkout() {
+      const workout = WorkoutService.getWorkoutByDate(this.humanReadableDate(this.date))
+      return workout ? workout.title : 'No workout'
+    }
+  },
+  methods: {
+    printDate(item) {
+      console.log(this.humanReadableDate(item.date))
+    },
+    humanReadableDate(timestamp) {
+      return `${timestamp.getFullYear()}-${timestamp.getMonth() + 1}-${timestamp.getDate()}`
+    },
+    changeDate(mode) {
+      let newDate
+      if (mode === 'now') {
+        newDate = new Date()
+      } else {
+        newDate = new Date(this.date)
+        if (mode === 'decrease') {
+          newDate.setDate(newDate.getDate() - 1)
+        } else if (mode === 'increase') {
+          newDate.setDate(newDate.getDate() + 1)
+        }
+      }
+      this.date = newDate
+    },
+  }
+}
 </script>
